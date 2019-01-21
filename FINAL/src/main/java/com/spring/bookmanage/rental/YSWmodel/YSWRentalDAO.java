bf38e29be94eea50612cabe918eaea6f3a65ae6a
user <user@DESKTOP-HDS19L2>
2019-01-21 17:54:29 +0900

    유세원 수정사항 FINAL

--- FINAL/src/main/java/com/spring/bookmanage/rental/YSWmodel/YSWRentalDAO.java
+++ FINAL/src/main/java/com/spring/bookmanage/rental/YSWmodel/YSWRentalDAO.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class YSWRentalDAO implements InterYSWRentalDAO {
 	
 	@Autowired
