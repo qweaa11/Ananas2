220cc6fa3f459c821ab9b2821c862f1ebd9a6e03
user <user@DESKTOP-CCBMHCS>
2019-01-22 15:32:41 +0900

    2019-01-22 유민후 반납목록 커밋, 아직 소팅은 안됨

--- FINAL/src/main/java/com/spring/bookmanage/returned/YMHmodel/InterYMHReturnedDAO.java
+++ FINAL/src/main/java/com/spring/bookmanage/returned/YMHmodel/InterYMHReturnedDAO.java
@@ -16,6 +16,34 @@ public interface InterYMHReturnedDAO
 	 * @return List<ReturnedVO>
 	 */
 	List<ReturnedVO> listReturned(HashMap<String, String> paraMap);
+
+	/**
+	 * 도서반납(returned)테이블에서 전체 카운트 조회해오기
+	 * 작성자 유민후
+	 *
+	 * @param HashMap<String, String> paraMap
+	 * @return int
+	 */
+	int getReturnedTotalCount(HashMap<String, String> paraMap);
+
+	
+	/**
+	 * 도서반납(returned)테이블에서 검색된 정보 조회해오기
+	 * 작성자 유민후
+	 *
+	 * @param HashMap<String, String> paraMap
+	 * @return List<ReturnedVO>
+	 */
+	List<ReturnedVO> listReturnedWithSearch(HashMap<String, String> paraMap);
+
+	/**
+	 * 도서반납(returned)테이블에서 검색될 카운트 조회해오기
+	 * 작성자 유민후
+	 *
+	 * @param HashMap<String, String> paraMap
+	 * @return int
+	 */
+	int getReturnedTotalCountWithSearch(HashMap<String, String> paraMap);
 	
 	
 	
--- FINAL/src/main/java/com/spring/bookmanage/returned/YMHservice/YMHReturnedService.java
+++ FINAL/src/main/java/com/spring/bookmanage/returned/YMHservice/YMHReturnedService.java
@@ -28,4 +28,74 @@ public class YMHReturnedService implements InterYMHReturnedService
 		List<ReturnedVO> returnedList = dao.listReturned(paraMap);
 		return returnedList;
 	}// end of listReturned
+
+
+	/**
+	 * 도서반납(returned)테이블에서 전체 카운트 조회해오기
+	 * 작성자 유민후
+	 *
+	 * @param HashMap<String, String> paraMap
+	 * @return int
+	 */
+	@Override
+	public int getReturnedTotalCount(HashMap<String, String> paraMap) {
+		int totalCount = dao.getReturnedTotalCount(paraMap);
+		return totalCount;
+	}
+
+	/**
+	 * 도서반납(returned)테이블에서 검색된 정보 조회해오기
+	 * 작성자 유민후
+	 *
+	 * @param HashMap<String, String> paraMap
+	 * @return List<ReturnedVO>
+	 */
+	@Override
+	public List<ReturnedVO> listReturnedWithSearch(HashMap<String, String> paraMap) {
+		List<ReturnedVO> returnedList = dao.listReturnedWithSearch(paraMap);
+		return returnedList;
+	}
+
+
+	/**
+	 * 도서반납(returned)테이블에서 검색될 카운트 조회해오기
+	 * 작성자 유민후
+	 *
+	 * @param HashMap<String, String> paraMap
+	 * @return int
+	 */
+	@Override
+	public int getReturnedTotalCountWithSearch(HashMap<String, String> paraMap) {
+		int totalCount = dao.getReturnedTotalCountWithSearch(paraMap);
+		return totalCount;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
