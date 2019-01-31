44614642781e1349d551e5f2ff41b6aa3a62d13f
user1 <user1@DESKTOP-SI9RJ74>
2019-01-31 14:11:38 +0900

    주석 메일발송, 차트 (김기복)

--- FINAL/src/main/java/com/spring/bookmanage/chart/KGBservice/ChartKGBInterService.java
+++ FINAL/src/main/java/com/spring/bookmanage/chart/KGBservice/ChartKGBInterService.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public interface ChartKGBInterService {
 
+	/**
+	 * 검색 가능한 년도를 가져오는 메소드
+	 * @return
+	 */
 	int findAllreturnTerm();
-	// 검색 가능한 년도를 가져오는 메소드
 	
+	
+	/**
+	 * 년도의 월별 장르 차트 정보를 가져오는 메소드
+	 * @param currentyear
+	 * @return
+	 */
 	List<HashMap<String, Object>> findAllReturnGenreChart(String currentyear);
-	// 년도의 월별 장르 차트 정보를 가져오는 메소드
 
 	
 
--- FINAL/src/main/java/com/spring/schedule/KGBservice/InterKGBScheduleService.java
+++ FINAL/src/main/java/com/spring/schedule/KGBservice/InterKGBScheduleService.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 public interface InterKGBScheduleService {
 
 	/**
-	 * 연체 되어있는 회원의 아이디를 알아오는 메소드
+	 * 연체 되어있는 회원의 데이터를 알아오는 메소드
 	 * @return
 	 */
 	List<HashMap<String, String>> findAllRentalDeadlineOver();
